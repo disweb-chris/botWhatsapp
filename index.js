0c6ec35961ef089883d2c65c81a980a7558ebb31
Christian Henriquez <disweb@gmail.com>
2024-08-23 20:16:33 -0300

    Agregada ruta para la raíz '/'

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const axios = require('axios');
 
 app.use(bodyParser.json());
 
-// Ruta para recibir mensajes (Webhook)
+// Ruta para la raíz "/"
+app.get('/', (req, res) => {
+    res.send('¡Bienvenido al bot de WhatsApp!');
+});
+
+// Ruta para recibir mensajes de Webhook
 app.post('/webhook', (req, res) => {
     console.log(req.body);
     res.status(200).send('Webhook recibido');
